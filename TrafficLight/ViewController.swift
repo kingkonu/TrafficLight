5eb5b2b5236b3c3e8ae3a830b60c168c3a52c089
Alexey Kanaev <kingkonu@me.com>
2023-03-18 23:32:14 +0300

    Start to create logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -15,10 +15,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        redLight.layer.cornerRadius = 50
+        yellowLight.layer.cornerRadius = 50
+        greenLight.layer.cornerRadius = 50
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func changeLight(_ sender: Any) {
+
+    @IBAction func changeLight(_ sender: UIButton) {
+        if redLight.alpha != 1 {
+            redLight.alpha = 1
+            sender.setTitle("Next", for: .normal)
+        }
+
+        if redLight.alpha == 1 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        }
     }
 
 }
