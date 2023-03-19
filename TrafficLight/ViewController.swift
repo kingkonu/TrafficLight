04bae4b243459d4348566783e980391318a66380
Alexey Kanaev <kingkonu@me.com>
2023-03-19 13:59:04 +0300

    End create Button

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -12,23 +12,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var redLight: UIView!
     @IBOutlet weak var yellowLight: UIView!
     @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var startButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         redLight.layer.cornerRadius = 50
         yellowLight.layer.cornerRadius = 50
         greenLight.layer.cornerRadius = 50
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 10
     }
 
 
-    @IBAction func changeLight(_ sender: UIButton) {
+    @IBAction func changeLight() {
         if redLight.alpha != 1
             && yellowLight.alpha != 1
             && greenLight.alpha != 1
         {
             redLight.alpha = 1
-            sender.setTitle("Next", for: .normal)
+            startButton.setTitle("Next", for: .normal)
         } else if redLight.alpha == 1
                     && yellowLight.alpha != 1
                     && greenLight.alpha != 1
